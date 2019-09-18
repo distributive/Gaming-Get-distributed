72f5800654121b225c8b7c934b03940b8f296a9a
chrisn <c.noble@warwick.ac.uk>
2019-09-18 16:54:56 +0100

    Enhanced the search features. Fixed some css issues.

--- static/js/search.js
+++ static/js/search.js
@@ -1,53 +1,91 @@
 $(document).ready (function () {
 
   $("#search-bar").on ("input", function () {
-    var search = $("#search-bar").val ().toLowerCase ().split (",");
-    search = search.filter (function (e) {return e;});
-    search = search.map (e => e.toString ().replace (/\s+/g, ' '));
-    var games = $(".game-ticket");
+    queries = $(this).val ().split (",");
+    applySearch ();
+  });
 
-    games.each (function (index, element) {
-      if (search.length != 0)
+  $(".game-ticket-tag").click (function () {
+
+    let tag = $(this).text ().trim ().toLowerCase ();
+
+    if (event.ctrlKey)
+    {
+      if (!queries.includes (tag))
       {
-        var matches = 0;
+        queries.push (tag);
+      }
+    }
+    else if (event.shiftKey)
+    {
+      queries = [];
+    }
+    else
+    {
+        queries = [tag];
+    }
 
-        // Check for matching name
-        var name = $(element).find ("h3").text ().toLowerCase ();
+    applySearch ();
+    showSearch ();
+  });
+
+});
+
+
+
+let queries = [];
+function applySearch ()
+{console.log (queries);
+  search = queries.map (e => e.trim ().toLowerCase ());
+  search = search.filter (function (e) {return e;});
+  search = search.map (e => e.toString ().replace (/\s+/g, ' '));
+  var games = $(".game-ticket");
+
+  games.each (function (index, element) {
+    if (search.length != 0)
+    {
+      var matches = 0;
+
+      // Check for matching name
+      var name = $(element).find ("h3").text ().toLowerCase ();
+      for (var i = 0; i < search.length; i++)
+      {
+        if (search[i].trim () && name.includes (search[i].trim ()))
+        {
+          matches++;
+        }
+      }
+
+      // Check for matching tags
+      var tags = $(element).find (".game-ticket-tag");
+      tags.each (function (i, e) {
         for (var i = 0; i < search.length; i++)
         {
-          if (search[i].trim () && name.includes (search[i].trim ()))
+          if (search[i].trim () && $(e).text ().toLowerCase ().includes (search[0].trim ()))
           {
             matches++;
           }
         }
+      });
 
-        // Check for matching tags
-        var tags = $(element).find (".game-ticket-tag");
-        tags.each (function (i, e) {
-          for (var i = 0; i < search.length; i++)
-          {
-            if (search[i].trim () && $(e).text ().toLowerCase ().includes (search[0].trim ()))
-            {
-              matches++;
-            }
-          }
-        });
-
-        if (matches > 0)
-          $(element).show (100);
-        else
-          $(element).hide (150);
-      }
-      else
-      {
+      if (matches > 0)
         $(element).show (100);
-      }
-    });
-
-    /*// Sort games based on relevance
-    games.sort (function (a, b) {
-      return a.val () - b.val ();
-    });*/
+      else
+        $(element).hide (150);
+    }
+    else
+    {
+      $(element).show (100);
+    }
   });
 
-});
+  /*// Sort games based on relevance
+  games.sort (function (a, b) {
+    return a.val () - b.val ();
+  });*/
+}
+
+function showSearch ()
+{
+  $("#search-bar").val (queries.join (", "));
+}
